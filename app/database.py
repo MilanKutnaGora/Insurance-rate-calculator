955d72b5c266c918e78150c3d0b0e04f53a269f7
MilanKutnogorskiy <kutnogorskiy77@gmail.com>
2024-11-30 21:24:05 +0300

    change db

--- app/database.py
+++ app/database.py
@@ -1,13 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from os import getenv
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://user:password@db/cargo_insurance"
+# Загрузка переменных окружения из файла .env
+load_dotenv()
 
+# Получение URL базы данных из переменных окружения
+DATABASE_URL = getenv("DATABASE_URL", "postgresql://postgres:postgres@db/insurance")
+
+# Создание движка SQLAlchemy
 engine = create_engine(DATABASE_URL)
+
+# Создание сессии
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Создание базового класса для моделей
 Base = declarative_base()
 
+# Функция для получения сессии базы данных
 def get_db():
     db = SessionLocal()
     try:
